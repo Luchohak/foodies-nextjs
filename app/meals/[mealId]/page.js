269cbf4fbc0476e76fc7f4aeed70b6b3559ce086
Luchohak <a00958934@gmail.com>
2024-01-31 13:50:37 -0600

    Displaying meal details

--- app/meals/[mealId]/page.js
+++ app/meals/[mealId]/page.js
@@ -1,8 +1,34 @@
+import Image from "next/image";
+import { getMeal } from "@/lib/meals";
+import classes from "./page.module.css";
+
 export default function MealDetailPage({ params }) {
+  const meal = getMeal(params.mealId);
+
+  meal.instructions = meal.instructions.replace(/\n/g, "<br />");
+
   return (
-    <main>
-      <h1>Your mean</h1>
-      <p>{params.mealId}</p>
-    </main>
+    <>
+      <header className={classes.header}>
+        <div className={classes.image}>
+          <Image src={meal.image} alt={meal.title} fill />
+        </div>
+        <div className={classes.headerText}>
+          <h1>{meal.title}</h1>
+          <p className={classes.creator}>
+            by <a href={`mailto:${meal.creator_email}`}>{meal.creator}</a>
+          </p>
+          <p className={classes.summary}>{meal.summary}</p>
+        </div>
+      </header>
+      <main>
+        <p
+          className={classes.instructions}
+          dangerouslySetInnerHTML={{
+            __html: meal.instructions,
+          }}
+        ></p>
+      </main>
+    </>
   );
 }
